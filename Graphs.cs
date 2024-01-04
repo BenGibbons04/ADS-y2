53ccba1a99987b4e53fd4fd1dd051fdf9ede7a8b
BenGibbons04 <113983674+BenGibbons04@users.noreply.github.com>
2024-01-04 11:55:38 +0000

    Add files via upload

---
+++ Graphs.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace p4cs
+{
+    class Exercises
+    {
+        static int returnCP(int[,,] graph1){
+            //Critical Path is longest possible route throguh program
+            int result = 0;
+            int[] nodes = [];
+            for(int x = 0; -1= graph1[x][0]; x++){
+                int num_nodes = graph1.GetLength(x);
+                for(int y = 0; y< num_nodes; y++){
+                    nodes.append(graph1[x],[y],[1])
+                }
+                
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+        int[, ,] graph1 = {
+            {{1, 6}, {2, 4}, {3, 5}},
+            {{4, 1}}, 
+            {{4, 1}}, 
+            {{5, 2}},
+            {{6, 9}, {7, 7}},
+            {{7, 4}},
+            {{8, 2}},
+            {{8, 4}},
+            {{-1, 0}}};
+        }
+
+        int starting_point = 1;
+        int finish = 5;
+
+        int currentnode = 0;
+        int previousnode = 0;
+
+        while(currentnode != finsih){
+           if(currentnode = 0){
+            currentnode =1;
+           }
+
+           currentnode = graph1[]
+        }
+
+
+
+
+
+    }
+}
